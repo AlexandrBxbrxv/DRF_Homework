9d8d6702b0f0a0f7c6f09d7b25ae17ffd543fa6d
AlexandrBxbrxv <g1026@bk.ru>
2024-10-23 04:43:28 +0500

    + модель Lesson

--- materials/models.py
+++ materials/models.py
@@ -16,6 +16,18 @@ class Course(models.Model):
         verbose_name_plural = 'курсы'
 
 
+class Lesson(models.Model):
+    title = models.CharField(max_length=200, verbose_name='название')
+    preview = models.ImageField(upload_to='materials/lessons', **NULLABLE, verbose_name='превью')
+    description = models.TextField(**NULLABLE, verbose_name='описание')
+    video_link = models.CharField(max_length=200, **NULLABLE, verbose_name='ссылка на видео')
 
+    course = models.ForeignKey(Course, on_delete=models.SET_NULL, **NULLABLE, related_name='lesson_course',
+                               verbose_name='курс')
 
+    def __str__(self):
+        return self.title
 
+    class Meta:
+        verbose_name = 'урок'
+        verbose_name_plural = 'уроки'
