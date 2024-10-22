f7bc0bafd2a0c66fe62e9e1a6e67eed49399f294
AlexandrBxbrxv <g1026@bk.ru>
2024-10-23 04:31:48 +0500

    + модель Course

--- materials/models.py
+++ materials/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=200, verbose_name='название')
+    preview = models.ImageField(upload_to='materials/courses', **NULLABLE, verbose_name='превью')
+    description = models.TextField(**NULLABLE, verbose_name='описание')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'курс'
+        verbose_name_plural = 'курсы'
+
+
