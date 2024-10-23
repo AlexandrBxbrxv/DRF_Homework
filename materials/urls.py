12bc5348f373210a45c07d43fc16bcdec565b588
AlexandrBxbrxv <g1026@bk.ru>
2024-10-23 05:32:51 +0500

    + контроллер для создания Lesson

--- materials/urls.py
+++ materials/urls.py
@@ -1,10 +1,14 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from materials.views import CourseViewSet
+from materials.apps import MaterialsConfig
+from materials.views import CourseViewSet, LessonCreateAPI
+
+app_name = MaterialsConfig.name
 
 router = DefaultRouter()
 router.register(r'courses', CourseViewSet, basename='courses')
 
 urlpatterns = [
-
+    path('lesson/create/', LessonCreateAPI.as_view(), name='lesson_create'),
 ] + router.urls
--- materials/views.py
+++ materials/views.py
@@ -1,9 +1,14 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 
 from materials.models import Course
-from materials.serializers import CourseSerializer
+from materials.serializers import CourseSerializer, LessonSerializer
 
 
 class CourseViewSet(viewsets.ModelViewSet):
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
+
+
+# CRUD для Lesson ###################################################
+class LessonCreateAPI(generics.CreateAPIView):
+    serializer_class = LessonSerializer
