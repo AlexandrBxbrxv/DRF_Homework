84530cb4a53aca25a7923337109309b1d1783503
AlexandrBxbrxv <g1026@bk.ru>
2024-10-23 05:59:28 +0500

    + путь до users

--- config/urls.py
+++ config/urls.py
@@ -21,5 +21,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('materials/', include('materials.urls', namespace='materials'))
+    path('materials/', include('materials.urls', namespace='materials')),
+    path('users/', include('users.urls', namespace='users')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
