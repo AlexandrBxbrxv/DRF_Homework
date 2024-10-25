aefe6ca8e4e7c64f7f88d3158d67cc191760e67b
AlexandrBxbrxv <g1026@bk.ru>
2024-10-25 17:28:09 +0500

    + поле payments в UserSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -3,14 +3,15 @@ from rest_framework import serializers
 from users.models import User, Payment
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = '__all__'
-
-
 class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payment
         fields = '__all__'
 
+
+class UserSerializer(serializers.ModelSerializer):
+    payments = PaymentSerializer(source='payment_user', many=True)
+
+    class Meta:
+        model = User
+        fields = '__all__'
