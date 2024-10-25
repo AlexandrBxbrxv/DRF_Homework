6338fed06bfd45e5a1554af57ac5a31034999d36
AlexandrBxbrxv <g1026@bk.ru>
2024-10-25 15:24:59 +0500

    CourseDetailSerializer добавлено поле lessons

--- materials/serializers.py
+++ materials/serializers.py
@@ -3,6 +3,12 @@ from rest_framework import serializers
 from materials.models import Course, Lesson
 
 
+class LessonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = '__all__'
+
+
 class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
@@ -11,17 +17,12 @@ class CourseSerializer(serializers.ModelSerializer):
 
 class CourseDetailSerializer(serializers.ModelSerializer):
     course_lessons_count = serializers.SerializerMethodField()
+    lessons = LessonSerializer(source='lesson_course', many=True)
 
     class Meta:
         model = Course
-        fields = ('title', 'preview', 'description', 'course_lessons_count',)
+        fields = ('title', 'preview', 'description', 'course_lessons_count', 'lessons',)
 
     @staticmethod
     def get_course_lessons_count(course):
         return Lesson.objects.filter(course=course).count()
-
-
-class LessonSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Lesson
-        fields = '__all__'
