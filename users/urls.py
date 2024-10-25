03d08ad5cf20238f4e5be119c00402c661be6f6a
AlexandrBxbrxv <g1026@bk.ru>
2024-10-25 17:33:43 +0500

    + ссылка на контроллер просмотра списка User

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from users.apps import UsersConfig
-from users.views import UserUpdateAPI, PaymentListAPI
+from users.views import UserUpdateAPI, PaymentListAPI, UserListAPI
 
 app_name = UsersConfig.name
 
 urlpatterns = [
+    path('user/list/', UserListAPI.as_view(), name='user_list'),
     path('user/update/<int:pk>/', UserUpdateAPI.as_view(), name='user_update'),
 
     path('payment/list/', PaymentListAPI.as_view(), name='payment_list'),
