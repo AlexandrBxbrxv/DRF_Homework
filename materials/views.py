5c6cffe68801ed848d7052eed381ad0dcaa566c4
AlexandrBxbrxv <g1026@bk.ru>
2024-10-28 15:21:22 +0500

    + permission IsAuthenticated for every controller

--- materials/views.py
+++ materials/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets, generics
+from rest_framework.permissions import IsAuthenticated
 
 from materials.models import Course, Lesson
 from materials.serializers import CourseSerializer, LessonSerializer, CourseDetailSerializer
@@ -6,6 +7,7 @@ from materials.serializers import CourseSerializer, LessonSerializer, CourseDeta
 
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
+    permission_classes = [IsAuthenticated]
 
     def get_serializer_class(self):
         if self.action == 'retrieve':
@@ -16,22 +18,27 @@ class CourseViewSet(viewsets.ModelViewSet):
 # CRUD для Lesson ###################################################
 class LessonCreateAPI(generics.CreateAPIView):
     serializer_class = LessonSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class LessonListAPI(generics.ListAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = [IsAuthenticated]
 
 
 class LessonRetrieveAPI(generics.RetrieveAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = [IsAuthenticated]
 
 
 class LessonUpdateAPI(generics.UpdateAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = [IsAuthenticated]
 
 
 class LessonDestroyAPI(generics.DestroyAPIView):
     queryset = Lesson.objects.all()
+    permission_classes = [IsAuthenticated]
