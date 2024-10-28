d0ffa66f1319c513b0d294efe466e4283806a261
AlexandrBxbrxv <g1026@bk.ru>
2024-10-28 15:04:19 +0500

    + token, token_refresh urls

--- users/urls.py
+++ users/urls.py
@@ -1,3 +1,8 @@
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 from django.urls import path
 from users.apps import UsersConfig
 from users.views import UserUpdateAPI, PaymentListAPI, UserListAPI
@@ -5,6 +10,9 @@ from users.views import UserUpdateAPI, PaymentListAPI, UserListAPI
 app_name = UsersConfig.name
 
 urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
     path('user/list/', UserListAPI.as_view(), name='user_list'),
     path('user/update/<int:pk>/', UserUpdateAPI.as_view(), name='user_update'),
 
