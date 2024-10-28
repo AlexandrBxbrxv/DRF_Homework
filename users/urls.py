6f62350d38dad9eb7a411c06d383eff72a38ca2d
AlexandrBxbrxv <g1026@bk.ru>
2024-10-28 15:19:41 +0500

    + urls for User CRUD

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,7 @@ from rest_framework_simplejwt.views import (
 
 from django.urls import path
 from users.apps import UsersConfig
-from users.views import UserUpdateAPI, PaymentListAPI, UserListAPI
+from users.views import UserUpdateAPI, PaymentListAPI, UserListAPI, UserCreateAPI, UserRetrieveAPI, UserDestroyAPI
 
 app_name = UsersConfig.name
 
@@ -13,8 +13,11 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
+    path('user/create/', UserCreateAPI.as_view(), name='user_create'),
     path('user/list/', UserListAPI.as_view(), name='user_list'),
+    path('user/detail/<int:pk>/', UserRetrieveAPI.as_view(), name='user_detail'),
     path('user/update/<int:pk>/', UserUpdateAPI.as_view(), name='user_update'),
+    path('user/delete/<int:pk>/', UserDestroyAPI.as_view(), name='user_delete'),
 
     path('payment/list/', PaymentListAPI.as_view(), name='payment_list'),
 ]
