4d09f2e45e2dfa8599dd818d6db120e2db536b9c
AlexandrBxbrxv <g1026@bk.ru>
2024-11-14 19:44:55 +0500

    + поле last_update в Course

--- materials/models.py
+++ materials/models.py
@@ -9,9 +9,10 @@ class Course(models.Model):
     title = models.CharField(max_length=200, verbose_name='название')
     preview = models.ImageField(upload_to='materials/courses', **NULLABLE, verbose_name='превью')
     description = models.TextField(**NULLABLE, verbose_name='описание')
+    last_update = models.DateTimeField(**NULLABLE, verbose_name='последнее обновление')
 
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, **NULLABLE, related_name='course_owner',
-                              verbose_name='владелец')
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, **NULLABLE,
+                              related_name='course_owner', verbose_name='владелец')
 
     def __str__(self):
         return self.title
